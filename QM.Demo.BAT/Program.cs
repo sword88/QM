c8a3a17d9b81c246608db6278257d5e03694b17d
david <12ljx12@163.com>
2017-09-30 14:29:01 +0800

    demo update

---
+++ QM.Demo.BAT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace QM.BAT
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main(string[] parms)
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(parms));
+        }
+    }
+}
