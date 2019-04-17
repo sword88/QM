a168041841d34b5c32ffe1b88312a05ff04e06db
david <12ljx12@163.com>
2019-04-17 14:56:10 +0800

    add demo  rabbitmq  rpc

---
+++ QM.Demo.RabbitMQ/RPCServer.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace QM.Demo.RabbitMQ
+{
+    class RPCServer
+    {
+        public static void Main()
+        {            
+
+            var factory = new ConnectionFactory() { HostName = "127.0.0.1" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "RPC_QUEUE", durable: false, exclusive: false, autoDelete: false, arguments: null);
+                    channel.BasicQos(0, 1, false);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    channel.BasicConsume(queue: "RPC_QUEUE", autoAck: false, consumer: consumer);
+
+
+                    Console.WriteLine("START SEVICES.....");
+
+                    consumer.Received += (model, ea) =>
+                     {
+                         string response = null;
+
+                         var body = ea.Body;
+                         var props = ea.BasicProperties;
+                         var replyProps = channel.CreateBasicProperties();
+                         replyProps.CorrelationId = props.CorrelationId;
+
+                         try
+                         {
+                             var message = Encoding.UTF8.GetString(body);                             
+                             response = dosomething(message);
+                             Console.WriteLine(response);
+                         }
+                         catch (Exception e)
+                         {
+                             response = "";
+                             Console.WriteLine(e.Message);
+                         }
+                         finally
+                         {
+                             var responseBytes = Encoding.UTF8.GetBytes(response);
+                             channel.BasicPublish(exchange: "", routingKey: props.ReplyTo,
+                               basicProperties: replyProps, body: responseBytes);
+                             channel.BasicAck(deliveryTag: ea.DeliveryTag,
+                               multiple: false);
+                         }
+
+                     };
+
+                    Console.ReadLine();
+                }
+            }
+        }
+
+        private static string dosomething(string msg)
+        {
+            QMDBHelper db = new QMDBHelper("DATA SOURCE=10.68.10.143:1537/db2dev;PASSWORD=mesft;USER ID=mesft");
+
+            string sql = "select Asewh_Func_IRCustlotNumRoll1('IR','1929','AIRF1404ZPBF00') from dual";
+
+            var obj = db.ExecuteScalar(CommandType.Text, sql, null);
+
+            return msg + obj.ToString();
+        }
+    }
+}
---
+++ QM.Demo.RabbitMQ_Client/RPCClient.cs
@@ -0,0 +1,86 @@
+﻿using Oracle.ManagedDataAccess.Client;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+using System.Threading;
+
+namespace QM.Demo.RabbitMQ_Client
+{
+    class RPCClient
+    {
+        public static void Main()
+        {
+            QMDBHelper db = new QMDBHelper("DATA SOURCE=10.68.10.143:1537/db2dev;PASSWORD=mesft;USER ID=mesft");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            var replyQueueName = channel.QueueDeclare().QueueName;
+            var consumer = new EventingBasicConsumer(channel);
+
+            var props = channel.CreateBasicProperties();
+            var correlationId = Guid.NewGuid().ToString();
+            props.CorrelationId = correlationId;
+            props.ReplyTo = replyQueueName;
+
+            BlockingCollection<string> respQueue = new BlockingCollection<string>();
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body;
+                var response = Encoding.UTF8.GetString(body);
+                if (ea.BasicProperties.CorrelationId == correlationId)
+                {
+                    respQueue.Add(response);
+                }
+            };
+
+            double i = 0;
+
+            while (true)
+            {
+                Thread.Sleep(1);
+
+                string message = "";
+
+                var messageBytes = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(
+                    exchange: "",
+                    routingKey: "RPC_QUEUE",
+                    basicProperties: props,
+                    body: messageBytes);
+
+                channel.BasicConsume(
+                        consumer: consumer,
+                        queue: replyQueueName,
+                        autoAck: true);
+
+                string result = respQueue.Take();
+
+                Console.WriteLine(result);
+
+                string sql = @"insert into BI_LOTLIST 
+                                    (aolot,
+                                     CUSTOMERLOT,
+                                     STEP) 
+                            values
+                                    (:ao,
+                                     :custlot,
+                                     'NA')";
+
+                OracleParameter[] param = new OracleParameter[]
+                {
+                    new OracleParameter(":ao",result),
+                    new OracleParameter(":custlot","A"),
+                };
+
+                db.ExecuteNonQuery(sql, CommandType.Text, param);
+            }
+        }
+    }
+}
